3f3c4e269c8c0093abbe2470ae53fd40d3e20cc3
Ioann <s10.dev@yandex.ru>
2021-04-01 15:06:34 +0300

    weekends fix

--- h.py
+++ h.py
@@ -28,19 +28,22 @@ def parse_schedule(url):
     message = f'Добрый вечер!\nПары на {day_title.text}:'
     for subject, time in zip(subjects, subjects_time):
         message += '\n\n' + time.text + '\n' + subject.text
-    return message
+    return (message, day_title.text)
 
 
 def check_time(time_to_sen):
+    weekend = 0
     time_to_send = time_to_sen*60*60
     dt = datetime.datetime.today()
     dt_sec = dt.hour*60*60 + dt.minute*60 + dt.second
+    if dt.isoweekday()==7:
+        weekend = 86400
     if dt.hour == time_to_sen:
-        interval = 0 
+        interval = 0 + weekend
     elif dt_sec < time_to_send:
-        interval = time_to_send - dt_sec
+        interval = time_to_send - dt_sec + weekend
     elif dt_sec > time_to_send:
-        interval = 86400 - (dt_sec - time_to_send)
+        interval = 86400 - (dt_sec - time_to_send) + weekend
     return interval
 
 
@@ -50,9 +53,15 @@ def main():
         interval = check_time(17)
         if interval == 0:
             try:
-                send_message(parse_schedule(MAI))
-                print('wait 3600')
-                time.sleep(3600)
+                day = parse_schedule(MAI)[1]
+                message = parse_schedule(MAI)[0]
+                send_message(message)
+                dt = datetime.datetime.today()
+                
+                if dt.isoweekday()==5 and day=='Пн':
+                    time.sleep(172800)
+                else:
+                    time.sleep(3600)
 
             except Exception as e:
                 print(f'Бот упал с ошибкой: {e}')
@@ -60,7 +69,6 @@ def main():
                 continue
         print(f'wait {interval}')
         time.sleep(interval)
-        
 
 
 if __name__ == '__main__':
